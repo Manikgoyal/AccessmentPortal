001b84a37ef2693e5b0a2c33ad6d984dbf4cda8f
manik <manik@arcadiantechnocrats.com>
2014-01-24 13:13:18 +0530

    commit
    done

---
+++ AssesmentPortal/MainActivity.java
@@ -0,0 +1,27 @@
+package com.action.actionbar;
+
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.v4.widget.DrawerLayout;
+
+public class MainActivity extends Activity
+{
+    /** Called when the activity is first created. */
+    
+    DrawerLayout drawer;
+    
+    
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+        
+        
+        
+    }
+    
+    
+    
+}
